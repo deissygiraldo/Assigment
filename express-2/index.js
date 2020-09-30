4d45c954c2aab21e3722803aa38b4beee24e759d
Deissy <deissygiraldo@outlook.com>
2020-09-29 20:59:50 -0400

    first commit. working with express

---
+++ express-2/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express ();
+
+const port = 3000; 
+
+// metodo get es que permite hacer la busqueda desde el navegador.
+// el primer parametro es la ruta y el ultimo e smi funcion call back que debe tener siempre dos parametros.
+app.get("/",(req,res) => {
+    res.send("Esta es la ruta principal");
+    console.log(`${new Date(Date.now()).toTimeString()} ${req.method} ${req.path} ${req.ip}`);//get the time, methoth, path and ip of who is requesting my app.
+    /*console.log(req.headers);
+    console.log(req.method);
+    console.log(req.path);*/
+});
+
+app.get( "/users",(req,res) =>{
+    console.log(`${new Date(Date.now()).toTimeString()} ${req.method} ${req.path} ${req.ip}`);
+    res.send("Estsa es la ruta de ususarios")
+})
+
+
+app.listen(port, ()=>{
+    console.log("Servidor inciando...");
+});
