c7a4a26a3177b40844035956f287784dcbd5366a
Deissy <deissygiraldo@outlook.com>
2020-10-05 23:01:15 -0400

    created put and delete

--- express-2/index.js
+++ express-2/index.js
@@ -37,34 +37,82 @@ app.listen(port, () => {
     console.log("Servidor iniciado ...");
 });
 
+const users = [];
 const logMiddleware = (req, res, next) => {
     console.log(`${new Date(Date.now()).toTimeString()} ${req.method} ${req.path} ${req.ip}`);
     next();
 };
 
+app.use(logMiddleware); //log en toda la app
+app.use(express.json()); //recibir info en formato json
+
 app.get("/", logMiddleware, (req, res) => { 
     res.send("Esta es la ruta raiz");
 });
 
+//class 22th
 app.route("/users")
     .get((req,res)=>{
+        res.send(users);
 
     }) 
     .post((req,res)=>{
-
+        const user = {
+            name: req.body.name,
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+        };
+        const findUser = users.find(u => u.username === user.username);
+        if (findUser === undefined){
+            users.push(user);
+            res.status(200).send(`El usuario creado es: ${user.name}`);
+        }else{
+            res.status(500).send("El usuario ya existe");
+        }  
     }) 
-    .put((req,res)=>{
 
+
+    app.route("/users/:username")
+    .get((req, res) => {
+        const username = req.params.username;
+        const findUser = users.find(u => u.username === username);
+        if( findUser === undefined ){
+            res.status(500).send("El usuario consultado no existe");
+        }else{
+            res.status(200).send(findUser);
+        }
     })
-    .delete((req,res)=>{
-
-    });  
+    .put((req, res) => {
+        const username = req.params.username;
+        const user = {
+            name: req.body.name,
+            username: username,
+            email: req.body.email,
+            password: req.body.password
+        };
+        const findUser = users.find(u => u.username === username);
+        if( findUser === undefined ){
+            res.status(500).send("El usuario consultado no existe");
+        }else{
+            const elementsIndex = users.findIndex(u => u.username === username );
+            users[elementsIndex] = user;
+            res.status(200).send(users);
+        }
+    })    
+    .delete((req, res) => {
+        const username = req.params.username;
+        const findUser = users.find(u => u.username === username);
+        if( findUser === undefined ){
+            res.status(500).send("El usuario consultado no existe");
+        }else{
+            const result = users.filter(u => u.username !== username);
+            users = result;
+            res.status(200).send(users);
+        }
+    });
     
 
-app.get("/users/:id", logMiddleware, (req, res) => {
-    const id = req.params.id;
-    res.send(`Esta es la página del usuario ${id}`);
-});
 
 app.get("/tweets", logMiddleware, (req, res) => {
     const { id, username } = req.query;
@@ -72,5 +120,5 @@ app.get("/tweets", logMiddleware, (req, res) => {
 });
 
 
-//class sep 22 other methots
+
 
