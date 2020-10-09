21e0be2955e60a288c8309f2ffdcbb0db8c7ffdc
Deissy <deissygiraldo@outlook.com>
2020-10-09 18:28:34 -0400

    configur logs middleware

--- express-2/API/middlewares/log/index.js
+++ express-2/API/middlewares/log/index.js
@@ -1,9 +1,18 @@
+const fs = require("fs")
 const log = (req, res, next) => {
     const date = new Date(Date.now()).toTimeString();
     const method = req.method;
     const path = req.path;
-    const ip = req.ip
-    console.log(`${date} ${method} ${path} ${ip}`);
+    const ip = req.ip;
+
+    const linea = `${date} ${method} ${path} ${ip}\n`;
+
+    const archivo = fs.createWriteStream("./logs/access.log", { 'flags': 'a'});
+    archivo.once("open", (f) => {
+        archivo.write(linea);
+    });
+
+    console.log(linea);
     next();
 };
 
