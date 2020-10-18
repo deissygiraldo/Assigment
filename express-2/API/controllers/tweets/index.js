8c19aae76fe2674020a47cbe1309363cd497746e
Deissy <deissygiraldo@outlook.com>
2020-10-18 19:07:48 -0400

    twitter module

--- express-2/API/controllers/tweets/index.js
+++ express-2/API/controllers/tweets/index.js
@@ -1,6 +1,8 @@
+const Twitter = require("twitter");
 const tweets = require("./../../models/tweets");
 const lib = require("./../../lib/dates");
 const response = require("./../../lib/response");
+const config = require("../../../config");
 
 const getTweets = (req, res)=>{
     res.send(tweets);
@@ -25,4 +27,20 @@ const getTweet = (req, res) => {
     }
 };
 
-module.exports = {getTweets, getTweet, newTweet};
+const getTweetsStream = (req, res) => {
+    const username = req.params.username;
+    const client = new Twitter({
+        consumer_key: config.twitter.consumerKey,
+        consumer_secret: config.twitter.consumerSecret,
+        access_token_key: config.twitter.accessTokenKey,
+        access_token_secret: config.twitter.accessTokenSecret
+      });
+    client.get("statuses/user_timeline", {screen_name: username}, (err, tweets, reponse) => {
+        if (err) {
+            res.status(500).json(response(false, undefined, [ {message: "Ocurrió un error"}]));
+        } else
+            res.status(200).json(response(true, tweets));
+    });  
+};
+
+module.exports = {getTweets, getTweet, newTweet, getTweetsStream}; 
